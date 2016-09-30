2d0118ea52403a897ca5d91bdafb664681593dd9
ChrisKarpinski <christopher.karpinski@ocsbstudent.ca>
2016-09-30 12:45:11 -0400

    Create CoxallStackMain.java

---
+++ CoxallStackMain.java
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *
+ * Created by: Chris Karpinski
+ * Created on: Sept 2016
+ * This program pushes something into the stack and pops it out
+ *
+ ****************************************************************************/
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner read = new Scanner(System.in);
+		MCoxallStack aStack = new MCoxallStack();
+		
+		String stackString;
+		String returnedString;
+		System.out.println("Enter the string");
+		stackString = read.next();
+		
+		aStack.push(stackString);
+		System.out.println("The top string was: " + aStack.pop());
+		
+	}
+
+}
